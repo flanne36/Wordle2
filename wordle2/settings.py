e8d695933c03b4f718c412e91ce1938d407354e2
flanne36 <flanne36@students.rowan.edu>
2022-08-11 10:26:16 -0400

    none

--- wordle2/settings.py
+++ wordle2/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'game'
+    'game',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+                os.path.join(BASE_DIR, 'static'),
+            ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
